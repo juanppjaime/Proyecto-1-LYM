d874c759521dd5b7e56e6b76e84b6559c9e767ce
juanppjaime <j.penaj@uniandes.edu.co>
2023-03-17 17:30:30 -0500

    Primeros cambios

--- Parser.java
+++ Parser.java
@@ -10,140 +10,171 @@ Modificado por Silvia Takahashi
  **/
 
 
-options 
-{
-  
-	LOOKAHEAD=1; 
-	IGNORE_CASE=true;
-	STATIC = false;
-	
-}
-   
-
-PARSER_BEGIN(Robot)
-
-package uniandes.lym.robot.control;
-
-import uniandes.lym.robot.kernel.*;
-import uniandes.lym.robot.view.Console;
+ options 
+ {
+   
+ LOOKAHEAD=1; 
+ IGNORE_CASE=true;
+ STATIC = false;
  
-import java.awt.Point;
-import java.io.*;
-import java.util.Vector;
-import java.util.LinkedList;
-
-@SuppressWarnings("serial")
-public class Robot 
-{
-
-
-	private RobotWorldDec world;
-	
-	
-	void setWorld(RobotWorld w) {
-		world = (RobotWorldDec) w;	
-	}
-
-	String salida=new String();
-}
-PARSER_END(Robot)
-
-SKIP:
-{
-	" "
-	|	"\r"
-	|	"\t"
-	| "\n"
-}
-
-TOKEN: /* Nombres de Comandos */
-{
-
-			<MOV:  "Mov">
-		| 	<RIGHT: "RIGHT">
-		| 	<PUT: "Put">
-		| 	<PICK: "Pick">
-		|  < POP: "Pop" >
-		|  <GO: "GO" >
-        |  < HOP:  "HOP" >		
-
-}
-
-TOKEN:
-{
-		<BALLOONS:  "BALLOONS" >
-		|	<CHIPS:     "CHIPS"  >	
-
-}
-
-
-
-TOKEN :
-{
-		<NUM: (<DIGIT>)+ >
-		|  	<#DIGIT: ["0"-"9"] >
-        
-}
-
-
-
-	//boolean command(uniandes.lym.robot.view.Console sistema) :
-	boolean command(Console sistema):
-	{	
-		
-		int x,y;
-		salida=new String();	
-	}
-
-	
-	{
-		 <EOF> {return false;} 
-	}
-
-	void put() :
-	{
-		int f=1;	
-	}
-	{
-		( <CHIPS>    "," f=num() {world.putChips(f); salida = "Command:  Put Chips"; })
-		|  	  ( <BALLOONS>   "," f=num() {world.putBalloons(f); salida = "Command:  Put Balloons";})	 
-
-	}
-
-	void get() :
-	{
-		int f=1;	
-	}
-	{
-		( <CHIPS>   "," f=num() {world.pickChips(f);salida = "Command:  Pick chips";})
-		|  	  ( <BALLOONS>   "," f=num() {world.grabBalloons(f);salida="Command:  Pick balloons";})	 
-
-	}
-
-	
-	
-
-	/**
-	 * Unsigned decimal number
-	 * @return the corresponding value of the string
-	 * @error  corresponding value is too large
-	 */
-
-	int num() throws Error:
-	{	
-		int total=1;
-	}
-	{
-		<NUM>
-		{
-			try 
-			{
-				total = Integer.parseInt(token.image);
-			} 
-			catch (NumberFormatException ee) 
-			{
-				throw new Error("Number out of bounds: "+token.image+" !!");
-			}
-			return total;
-		}
-	}
+ }
+    
+ 
+ PARSER_BEGIN(Robot)
+ 
+ package uniandes.lym.robot.control;
+ 
+ import uniandes.lym.robot.kernel.*;
+ import uniandes.lym.robot.view.Console;
+  
+ import java.awt.Point;
+ import java.io.*;
+ import java.util.Vector;
+ import java.util.LinkedList;
+ 
+ @SuppressWarnings("serial")
+ public class Robot 
+ {
+ 
+ 
+ private RobotWorldDec world;
+ private HashMap<String, Integer> variables= new HashMap<String, Integer>();
+ 
+ void setWorld(RobotWorld w) {
+ world = (RobotWorldDec) w;
+ }
+ 
+ String salida=new String();
+ }
+ PARSER_END(Robot)
+ 
+ SKIP:
+ {
+ " "
+ |"\r"
+ |"\t"
+ | "\n"
+ }
+ 
+ TOKEN: /* Nombres de Comandos */
+ {
+ <GOTO:"goto">
+ |   <MOVEINDIR: "moveInDir">
+ |<MOVE: "move">
+ |<TURN: "turn">
+ |   <FACE: "face">
+ | <PUT: "put">
+ |<PICK: "pick">
+ |<MOVETOTHE: "moveToThe">
+ |<JUMPTOTHE: "jumpToThe">
+ |<JUMPINDIR: "jumpInDir">
+ |<NOP: "nop">
+ 
+ 
+ }
+ 
+ TOKEN:
+ {
+ <NORTH: "north">
+ |<SOUTH: "south">
+ |<WEST: "west">
+ |<EAST: "east">
+ | <BALLONS: "Ballons">
+ |<CHIPS: "Chips">
+ 
+ }
+ 
+ 
+ 
+ TOKEN :
+ {
+ <NUM: (<DIGIT>)+ >
+ |  <#DIGIT: ["0"-"9"] >
+ |   <LETTER: ["a"-"z"]>
+ |   <NAME:(<LETTER>)+>
+         
+ }
+ 
+ 
+ 
+ //boolean command(uniandes.lym.robot.view.Console sistema) :
+ boolean command(Console sistema):
+ {
+ 
+ int x,y;
+ salida=new String();
+ }
+ 
+ 
+ {
+  <EOF> {return false;} 
+ }
+ 
+ void comandos():
+ {
+ int var1;
+ int var2;
+ String dir;
+ String word;
+ }
+ {
+ (
+ <GOTO> ":" var1=varOrNum()"," var2=varOrNum(){world.setPostion(var1,var2); word = "Command: goTo";}
+ |<MOVE> ":" var1=varOrNum()"," var2=0{world.setPostion(var1,var2); word = "Command: move";}
+ |<TURN> ":" (<NORTH>|<SOUTH>|<EAST>|<WEST>){world.turnRight(); word = "Command: turn" ;}
+ |<FACE> ":" dir=(<NORTH>|<SOUTH>|<EAST>|<WEST>){this.facing=dir; word = "Command: turn" ;}
+ 
+ )
+ }
+ int varOrNum():
+ {
+ int total=0;
+ }
+ {
+ (<NAME> {
+ variable = token.image;
+ if (variables.get(variable) != null) {
+ total= variables.get(variable);
+ }
+ else{
+ throw new Error("La variable dada no existe");
+ }
+  return total;}
+ |<NUM> {try
+ {
+ total = Integer.parseInt(token.image);
+ } 
+ catch (NumberFormatException ee) 
+ {
+ throw new Error("Number out of bounds: "+token.image+" !!");
+ }
+ return total}
+ )
+ 
+ }
+ 
+ 
+ /**
+  * Unsigned decimal number
+  * @return the corresponding value of the string
+  * @error  corresponding value is too large
+  */
+ 
+ int num() throws Error:
+ {
+ int total=1;
+ }
+ {
+ <NUM>
+ {
+ try 
+ {
+ total = Integer.parseInt(token.image);
+ } 
+ catch (NumberFormatException ee) 
+ {
+ throw new Error("Number out of bounds: "+token.image+" !!");
+ }
+ return total;
+ }
+ }
